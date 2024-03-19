45552e057d7bf4f2822210baa646516173b35377
Yann MLEKO <yann.mleko@etu.univ-grenoble-alpes.fr>
2024-03-19 16:37:28 +0100

    Updated arduino-cardinal-midi-interface
    Updated Python interface so it can handle multiple sensors connected to the Arduino card and send multiple MIDI messages to Cardinal on several MIDI ports

--- arduino-cardinal-midi-interface/arduino-cardinal-midi-interface.py
+++ arduino-cardinal-midi-interface/arduino-cardinal-midi-interface.py
@@ -13,38 +13,49 @@ def read_serial_data():
     return arduino.readline().decode('utf8', errors='ignore').strip()
 
 def process_data(data):
-    try:
-        cut_data = int(data) - 950
-        adjusted_data = cut_data * (1023 / (1023 - 950))
-        previous_data = adjusted_data
-        voltage = (adjusted_data / 1023) * 5
+    serial_output = data.split(',', 1)
+    if len(serial_output) == 2 and serial_output[0] != '' and serial_output[1] != '':
+        cut_data = int(serial_output[1]) - 950
+        scaled_data = cut_data * (1023 / (1023 - 950))
+        previous_data = scaled_data
+        voltage = (scaled_data / 1023) * 5
 
         if voltage >= 4.9:
             voltage = 4.9
         if voltage <= 0.1:
             voltage = 0.1
 
-        print("Raw voltage value : ", voltage)
+        processed_data = {}
+        processed_data['pin'] = serial_output[0]
+        processed_data['voltage'] = voltage
 
-        return voltage
-    except ValueError:
-        print("Invalid data")
-        return previous_data
+        return processed_data
+    else:
+        return 0
 
-def convert_to_MIDI(data):
-    if data:
-        print("display data for error : ", data)
-        data = int(data * 127 / 5)
-        print("MIDI converted voltage : ", data)
-    MIDI_message = mido.Message('control_change', control=3, value=data)
+def convert_to_MIDI_and_send(data):
+    data['voltage'] = int(data['voltage'] * 127 / 5)
+
+    match data['pin']:
+        case '0':
+            MIDI_message = mido.Message('control_change', control=1, value=data['voltage'])
+        case '1':
+            MIDI_message = mido.Message('control_change', control=2, value=data['voltage'])
+        case '2':
+            MIDI_message = mido.Message('control_change', control=3, value=data['voltage'])
+        case '3':
+            MIDI_message = mido.Message('control_change', control=4, value=data['voltage'])
+        case other:
+            print("No match found")
+
     outport.send(MIDI_message)
-    print("Sent MIDI message : ", MIDI_message)
     
 
 if __name__ == "__main__":
     while True:
-        data = read_serial_data()
+        serial_output = read_serial_data()
 
-        if data:
-            processed_data = process_data(data)
-            convert_to_MIDI(processed_data)
+        if serial_output:
+            processed_data = process_data(serial_output)
+            if processed_data:
+                convert_to_MIDI_and_send(processed_data)
