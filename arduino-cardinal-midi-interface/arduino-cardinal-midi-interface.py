93720dba1088e43507c337b9e053b70a75760c60
Yann MLEKO <yann.mleko@etu.univ-grenoble-alpes.fr>
2024-04-06 10:27:11 +0200

    Added documentation in Python code

--- arduino-cardinal-midi-interface/arduino-cardinal-midi-interface.py
+++ arduino-cardinal-midi-interface/arduino-cardinal-midi-interface.py
@@ -2,6 +2,7 @@ from time import sleep
 import serial
 import mido
 import jack
+import signal
 
 BAD_DATA = 0
 TIMEOUT = 30
@@ -41,13 +42,20 @@ playback_left = jack_client.get_port_by_name(AUDIO_PLAYBACK_LEFT)
 playback_right = jack_client.get_port_by_name(AUDIO_PLAYBACK_RIGHT)
 
 def get_control(control):
+    """
+    Returns channel corresponding to the MIDI Control Change (CC) gate on which to sent the MIDI message
+    :param control: The control change corresponding to the channel input to retrieve
+    """
+    
     return CONTROLS[control - 1]
 
 def check_connection(input_port, output_port):
-    """Checks if a connection is already established between two ports
+    """
+    Checks if a connection is already established between two ports
     :param input_port: The input port
     :param output_port: The output port
-    :return True if the connection is already established, False otherwise"""
+    :return True if the connection is already established, False otherwise
+    """
 
     connectionAlreadyBound = False
     for connection in jack_client.get_all_connections(input_port):
@@ -58,6 +66,12 @@ def check_connection(input_port, output_port):
     return connectionAlreadyBound
 
 def connect_interface_to_cardinal():
+    """
+    Checks if all connections are already made and does them if not. This includes:
+    - The MIDI connection between the interface MIDI output and Cardinal MIDI input
+    - The left and right audio connections between Cardinal audio outputs and the System left and right audio playback inputs
+    """
+
     if not check_connection(interface_midi_outport, cardinal_midi_inport):
         jack_client.connect(interface_midi_outport, cardinal_midi_inport)
 
@@ -70,6 +84,14 @@ def connect_interface_to_cardinal():
     print("Connection established")
 
 def stop_interface():
+    """
+    Stops the inteface when a shutting down signal is sent (e.i when a KeyboardInterruption such as CTRL-C is triggered)
+
+    It resets all Cardinal CC outputs to 0 by sending a 0 value to them, so the configuration is reset when the interface is shutting down
+
+    It also closes the MIDI and audio ports connections between the interface and Cardinal and closes the MIDI ports before exiting
+    """
+
     print("\rResetting all Cardinal control change outputs")
     for control in CONTROLS:
         reset_message = mido.Message('control_change', control=control, value=0)
@@ -98,6 +120,14 @@ def stop_interface():
     exit(0)
 
 def reconnect_serial():
+    """
+    Tries to reconnect the interface to the Arduino board when a deconnection occurs
+
+    The interface will regularly try to reconnect itself to the serial port the Arduino board was using, until a timeout is reached
+
+    A message will be displayed to inform the user whether the reconnection has succeeded or not
+    """
+
     print("Serial port disconnected")
     print("Trying to reconnect", end='', flush=True)
     global arduino_serial
@@ -122,6 +152,14 @@ def reconnect_serial():
         exit(0)
 
 def reconnect_cardinal():
+    """
+    Tries to reconnect the interface to Cardinal when a deconnection occurs
+
+    The interface will regularly try to reconnect its MIDI output port to the Arduino MIDI input port, until a timeout is reached
+
+    A message will be displayed to inform the user whether the reconnection has succeeded or not 
+    """
+
     print("Connection with Cardinal lost, trying to reconnect...")
     print("Trying to reconnect", end='', flush=True)
     timeout = 0
@@ -145,11 +183,29 @@ def reconnect_cardinal():
 
 
 def read_serial_data():
+    """
+    Reads the data that is sent on the serial port on which the Arduino board and the interface are connected
+
+    The data contains the device pin that has sent it and it's value
+    :return the raw data read on the serial port in the following format : device_id, value
+    """
+
     data = arduino_serial.readline().decode('utf8', errors='ignore').strip()
     arduino_serial.flushInput()
     return data
 
 def process_data(data):
+    """
+    Processes the data read on the serial port. It actually converts the value received from the serial port which is coded on 10 bits to a value corresponding on 7 bits (because Cardinal MIDI ports only accepts 7 bit data)
+
+    Also applies a scale factor to the values because of the portentiometers small range of changing values used during the development
+
+    The data is checked to see if it fits the defined format, which is : device_id, value. If it does the data is processed and if not the data is skipped
+
+    :param data: The raw data to be processed
+    :return the processed data if its format is valid or a BAD_DATA value if not
+    """
+
     serial_output = data.split(',')
     if len(serial_output) == 2 and serial_output[0] != '' and serial_output[1] != '' and serial_output[1].isdigit():
         cut_data = int(serial_output[1]) - 950
@@ -171,6 +227,16 @@ def process_data(data):
         return BAD_DATA
 
 def convert_to_MIDI_and_send(data):
+    """
+    Converts the processed value to a MIDI message and sends it to a defined MIDI output depending on the pin of the device
+
+    The signal will be received on the MIDI input corresponding to the MIDI output on which the data was sent
+
+    If the device pin corresponds to a valid MIDI output port the data will be sent on it. If not a message will be displayed indicating that no port matches the device pin
+
+    :param data: the processed data to be converted and sent as MIDI
+    """
+    
     data['voltage'] = int(data['voltage'] * 127 / 5)
 
     isMessageDataValid = True
@@ -184,7 +250,7 @@ def convert_to_MIDI_and_send(data):
             MIDI_message = mido.Message('control_change', control=get_control(3), value=data['voltage'])
         case other:
             isMessageDataValid = False
-            print("No match found")
+            print("No match found between the device pin:" + data['pin'] + " and the configured MIDI output ports")
         
     sleep(0.01)
 
@@ -192,6 +258,10 @@ def convert_to_MIDI_and_send(data):
         mido_outport.send(MIDI_message)
 
 def process_midi():
+    """
+    Groups the methods that handle the data from the reading to the sending
+    """
+    
     serial_output = read_serial_data()
     processed_data = process_data(serial_output)
     if processed_data:
